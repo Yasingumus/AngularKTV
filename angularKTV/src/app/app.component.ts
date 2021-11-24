389ddf0428f383587af9b136d5bf9ef8aff32f59
Yasingumus <yasin52gumus@gmail.com>
2021-11-24 10:01:47 +0100

    GET request voltooid

--- angularKTV/src/app/app.component.ts
+++ angularKTV/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  mockjson = 'http://echo.jsontest.com/key/value/one/two';
+
+  constructor(private http: HttpClient) {
+    this.http.get(this.mockjson).toPromise().then(data => {
+      console.log(data);
+      })
+  }
   title = "Kpi's";
 }
--- angularKTV/src/app/app.module.ts
+++ angularKTV/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,12 +14,12 @@ import { SingleKpiComponent } from './components/dashboard/single-kpi/single-kpi
     AppComponent,
     SingleKpiComponent,
     KpiDataComponent,
-    DashboardComponent
-
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
